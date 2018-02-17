498d99d406913576f371aead1e871428833b085e
Caio Maia <caaiomaia@gmail.com>
2018-02-17 02:46:40 -0200

    Show consumed message in consumer

--- src/main/kotlin/cnmaia/test/kafka/Listener.kt
+++ src/main/kotlin/cnmaia/test/kafka/Listener.kt
@@ -4,10 +4,11 @@ import org.springframework.kafka.annotation.KafkaListener
 import java.util.concurrent.CountDownLatch
 
 class Listener {
-    val latch : CountDownLatch = CountDownLatch(1)
+    private val latch : CountDownLatch = CountDownLatch(1)
 
     @KafkaListener(id = "first-listener", topics = ["fist"], groupId = "first-group")
     fun list(foo: String) {
+        println(foo)
         this.latch.countDown()
     }
 }
